55ee1b1960993fbf4046b49b0b9e7c5265ccb5df
Mixail <mixail.korchikov@mail.ru>
2022-05-02 17:54:14 +0300

    import scenes
    * Arkopong scene;
    * ArticTennis scene.

---
+++ Assets/Scripts/Car_Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Spawn : MonoBehaviour
+{
+    public GameObject[] cars;
+    public GameObject[] spawn;
+
+    private int randPose;
+    private int randCar;
+
+    public float spawnRate = 2f;
+    float nextSpawn = 0f;
+
+
+    void Update()
+    {
+        if(Time.time > nextSpawn)
+        {
+            randPose = Random.Range(0, spawn.Length);
+            randCar = Random.Range(0, cars.Length);
+            Debug.Log("Spawn");
+            Instantiate(cars[randCar], spawn[randPose].transform.position, spawn[randPose].transform.rotation);
+            nextSpawn = Time.time + spawnRate;
+        }
+    }
+}
---
+++ Assets/Scripts/car_move.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_move : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
+        Destroy(gameObject, 15);
+    }
+}
